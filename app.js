b097e7fbdf7d96756c3fa0e0492955241e5174e5
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-13 00:06:45 +0000

    should work on issue_comment.created

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
-  app.on("issues.opened", async (context) => {
+  app.on(["issues.opened", "issue_comment.created"], async (context) => {
     return context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
     );
