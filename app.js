e46570382200937b0177e730444ea53afc4b43ce
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-13 00:09:06 +0000

    removed recursive effect of comments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
   app.on(["issues.opened", "issue_comment.created"], async (context) => {
+    if (context.isBot) return;  // Don't want recursive - effect
     return context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
     );
