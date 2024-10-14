a5db8e1f5a95715416072bd47098959ee86490df
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-14 05:07:54 +0000

    Refactor GitHub webhooks configuration by adding authentication

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,6 +1,18 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 
 const app = require("../../../app");
-const probot = createProbot();
+const { Octokit } = require("@octokit/rest");
+const { createAppAuth } = require("@octokit/auth-app");
 
-module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+const octokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.WEBHOOK_SECRET,
+  },
+});
+
+
+module.exports = createNodeMiddleware(app, { octokit, webhooksPath: '/api/github/webhooks' });
